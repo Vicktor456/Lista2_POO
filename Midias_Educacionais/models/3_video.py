efbab8caa83ad86992dec238762cb05a5bc6f192
Vicktor <vitorlx456re2cod@gmail.com>
2026-05-22 18:06:10 -0400

    Delete Midias_Educacionais directory

--- Midias_Educacionais/models/3_video.py
+++
@@ -1,8 +0,0 @@
-class Video(Midia):
-    def __init__(self, titulo, duracao, resolucao):
-        super().__init__(titulo, duracao)
-
-        self.resolucao = resolucao
-
-    def reproduzir(self):
-        return f"Reproduzindo o video {self.titulo} com duração de {self.duracao} na resolução de {self.resolucao}"
--- Midias_Educacionais/models/5_texto_narrado.py
+++
@@ -1,8 +0,0 @@
-class TextoNarrado(Midia):
-    def __init__(self, titulo, duracao, idioma):
-        super().__init__(titulo, duracao)
-
-        self.idioma = idioma
-
-    def reproduzir(self):
-        return f"Reproduzindo o texto narrado {self.titulo} com duração de {self.duracao} no idioma {self.idioma}"
