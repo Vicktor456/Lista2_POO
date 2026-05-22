1fe864ec24758aea76da48a64df6adf5679ee8c1
Vicktor <vitorlx456re2cod@gmail.com>
2026-05-22 18:23:19 -0400

    Create Funcionario class with basic attributes

---
+++ 2_Funcionarios_Empresa/models/2_funcionario.py
@@ -0,0 +1,11 @@
+class Funcionario(ABC):
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self.cpf = cpf
+
+    def mostrar_dados(self):
+        return f"Nome: {self.nome} | CPF: {self.cpf}"
+    
+    @abstractmethod
+    def calcular_pagamento(self):
+        pass
