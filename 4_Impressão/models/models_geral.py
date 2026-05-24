9f6ab33410780ced266dfaff429bd522fbc84ba7
Vicktor <vitorlx456re2cod@gmail.com>
2026-05-24 11:31:24 -0400

    Add classes for printing Boleto, Etiqueta, and Relatorio

---
+++ 4_Impressão/models/models_geral.py
@@ -0,0 +1,26 @@
+class Imprimivel(Protocol):
+    def imprimir(self) -> None:
+        ...
+
+class Boleto:
+    def __init__(self, codigo, valor):
+        self.codigo = codigo
+        self.valor = valor
+        
+    def imprimir(self) -> None:
+        return f"Boleto - Codigo: {self.codigo} | Valor: R${self.valor}"
+
+class Etiqueta:
+    def __init__(self, destinatario, endereco):
+        self.destinatario = destinatario
+        self.endereco = endereco
+
+    def imprimir(self) -> None:
+        return f"Etiqueta - Destinatario: {self.destinatario} | Endereço: {self.endereco}"
+
+class RelatorioSimples:
+    def __init__(self, titulo):
+        self.titulo = titulo
+
+    def imprimir(self) -> None:
+        return f"Relatorio Simples - Titulo: {self.titulo}"
